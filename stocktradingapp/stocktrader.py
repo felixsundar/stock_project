d12a4ff85e22ea0d0b2a2fd2a00b9d79283bf22f
Felix Sundar <17445733+felixsundar@users.noreply.github.com>
2020-03-25 16:44:33 +0530

    tick analysis complete. except dynamic stoploss

--- stocktradingapp/stocktrader.py
+++ stocktradingapp/stocktrader.py
@@ -1,5 +1,5 @@
 import logging
-from collections import deque
+from queue import PriorityQueue
 
 from django.contrib.auth.models import User
 from kiteconnect import KiteConnect
@@ -9,11 +9,12 @@ from stocktradingapp.models import Stock
 
 logging.basicConfig(filename=settings.LOG_FILE_PATH, level=logging.DEBUG)
 
-current_positions = []
+current_positions = {} # list of length 2 for each position. list[0]: entry price. list[1]: stoploss
 pending_orders = []
 fund_available = 0.0
 token_symbol_map = {}
-price_windows = {}
+trigger_prices = {}
+signal_queue = PriorityQueue(maxsize=5)
 
 def analyzeTicks(tick_queue):
     kite = createKiteConnect()
@@ -23,31 +24,27 @@ def analyzeTicks(tick_queue):
     while True:
         tick = tick_queue.get(True)
         for instrument in tick:
-            price_window = price_windows[instrument['instrument_token']]
-            dump = price_window.popleft()
-            price_window.append(instrument['last_price'])
-            logging.debug(price_window)
-            signal = checkForEntry(list(price_window))
-            if signal != 0:
-                # sendEntrySignal(instrument['instrument_token'], signal)
-                logging.debug('\n\n\n\n\nsignal received - {}\n\n\n\n\n'.format(signal))
+            instrument_token = instrument['instrument_token']
+            current_price = instrument['last_price']
+            checkEntryTrigger(instrument_token, current_price)
+            checkStoploss(instrument_token, current_price)
 
-def checkForEntry(price_list):
-    for i in range(5,10):
-        if price_list[i] <= price_list[i-1]:
-            break
-    else:
-        return 1 #long entry signal
-    for i in range(5,10):
-        if price_list[i] >= price_list[i-1]:
-            break
-    else:
-        return 2 #short entry signal
-    return 0
-    # pointfivepercent = 45
-    # latest_price = price_list[9]
-    # for i in range(4,9):
-    #     if latest_price - price_list[i] >=
+def checkEntryTrigger(instrument_token, current_price):
+    trigger_price = trigger_prices[instrument_token]
+    if current_price < trigger_price:  # entry trigger breached
+        trigger_prices[instrument_token] = 0.995 * current_price
+        sendSignal(1, instrument_token)
+    else:  # update entry trigger
+        trigger_prices[instrument_token] = max(trigger_price, 0.995 * current_price)
+
+def checkStoploss(instrument_token, current_price):
+    current_position = current_positions.get(instrument_token)
+    if current_position is None:
+        return
+    if current_price >= current_position[1]:  # stoploss breached
+        sendSignal(0, instrument_token)
+    else:  # update stoploss
+        current_position[1] = min(current_position[1], 1.005 * current_price)
 
 def createKiteConnect():
     user = User.objects.get_by_natural_key(settings.PRIMARY_USERNAME)
@@ -62,4 +59,4 @@ def setupTokenSymbolMap():
     stocks = Stock.objects.filter(active=True)
     for stock in stocks:
         token_symbol_map[stock.instrument_token] = stock.trading_symbol
-        price_windows[stock.instrument_token] = deque(iterable=[0 for i in range(10)], maxlen=10)
+        trigger_prices[stock.instrument_token] = 0.0 # trigger price
