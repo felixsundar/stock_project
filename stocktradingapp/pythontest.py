cdd97002a09ca7a14202258a27f4bf0f3ca2b409
Felix Sundar <17445733+felixsundar@users.noreply.github.com>
2020-04-16 12:16:56 +0530

    est

--- stocktradingapp/pythontest.py
+++ stocktradingapp/pythontest.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from datetime import datetime
 from time import sleep
 
@@ -9,8 +10,10 @@ from stock_project import settings
 logging.basicConfig(filename=settings.LOG_FILE_PATH, level=logging.DEBUG)
 
 def runTests():
-    schedule.every().day.at('11:22').do(logtime)
+    schedule.every().day.at('12:18').do(logtime)
     logging.debug('schedule started...')
+    logging.debug('tiMe - {}'.format(time.time()))
+    logging.debug('timezOne - {}'.format(time.timezone))
     while True:
         schedule.run_pending()
         sleep(1)
